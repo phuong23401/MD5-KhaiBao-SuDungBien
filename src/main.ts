270f62389168f2e8df5ead6dfeffed669f1e4760
phuong23401 <hoaphuong23401@gmail.com>
2021-07-14 10:34:41 +0700

    add

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+let width : number;
+let height : number;
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+width = 10.5;
+height = 20;
 
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+let area : number = width * height;
+console.log(`Area of rectangle: ${area}`);
